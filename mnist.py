25d7972ddfaf8c133db5d3b639f62e217af2998c
pecasruiz <pedro-casado@hotmail.es>
2021-07-12 10:57:17 +0200

    RE-shape de las imagens

--- mnist.py
+++ mnist.py
@@ -1,7 +1,20 @@
-import keras
+import numpy as np
+from tensorflow import keras
+# from tensorflow.keras import layers
 
 (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
 assert x_train.shape == (60000, 28, 28)
 assert x_test.shape == (10000, 28, 28)
 assert y_train.shape == (60000,)
 assert y_test.shape == (10000,)
+
+# Pasamos la imagen de prayscale a binaria
+x_train = x_train.astype("float32") / 255
+x_test = x_test.astype("float32") / 255
+
+# Ver dimensiones de las imagenes
+x_train = np.expand_dims(x_train, -1)
+x_test = np.expand_dims(x_test, -1)
+print("x_train shape:", x_train.shape)
+print(x_train.shape[0], "train samples")
+print(x_test.shape[0], "test samples")
