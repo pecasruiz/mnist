dd9d4bd93293e0ef9b7e8c94481f5ff6fc9f6140
pecasruiz <pedro-casado@hotmail.es>
2021-07-12 13:26:53 +0200

    Añador el modelo y compilo

--- mnist.py
+++ mnist.py
@@ -1,20 +1,37 @@
-import numpy as np
 from tensorflow import keras
-# from tensorflow.keras import layers
+from matplotlib import pyplot as plt
 
 (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
-assert x_train.shape == (60000, 28, 28)
-assert x_test.shape == (10000, 28, 28)
-assert y_train.shape == (60000,)
-assert y_test.shape == (10000,)
+print(x_train.shape)  # (60000, 28, 28)
+print(x_test.shape)
+class_names = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 # Pasamos la imagen de prayscale a binaria
 x_train = x_train.astype("float32") / 255
 x_test = x_test.astype("float32") / 255
 
-# Ver dimensiones de las imagenes
-x_train = np.expand_dims(x_train, -1)
-x_test = np.expand_dims(x_test, -1)
-print("x_train shape:", x_train.shape)
-print(x_train.shape[0], "train samples")
-print(x_test.shape[0], "test samples")
+# Representamos los digitos
+plt.figure(figsize=(10, 10))
+for i in range(25):
+    plt.subplot(5, 5, i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(x_train[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[y_train[i]])
+plt.show()
+
+# Flatten images
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(10, activation='softmax')
+])
+
+# Compilamos el modelo
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+# Entrenamos
+model.fit(x_train, y_train, epochs=10)
