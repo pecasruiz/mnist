c647358684ec5fcad48df5d89e48ed8aaa54c7db
pecasruiz <pedro-casado@hotmail.es>
2021-07-14 09:34:30 +0200

    Creamos archivo Convolutional NN

--- mnist.py
+++ mnist.py
@@ -22,7 +22,8 @@ model.compile(optimizer='adam',
               metrics=['accuracy'])
 
 # Entrenamos
-history = model.fit(x_train, y_train, epochs=10)
+history = model.fit(x_train, y_train, epochs=10,
+                    batch_size=100, validation_split=0.2)
 
 # Comprobamos la precision
 test_loss, test_acc = model.evaluate(x_test,  y_test, verbose=2)
